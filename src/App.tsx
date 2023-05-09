65425bea4b352720ccba459301f605ca9d0b1b09
lippiece <github@lippiece.anonaddy.me>
2023-05-09 17:02:55 +0200

    setup .env

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
 // eslint-disable sort-keys-fix/sort-keys-fix
-import { Suspense } from "react"
-// import * as dotenv from 'dotenv'
-import { Outlet }   from "react-router-dom"
-
-import Footer from "./components/Footer"
-import Nav    from "./components/Nav"
+import '@noshot/env'
+import { initializeApp } from "firebase/app"
+import { Suspense }      from "react"
+import { Outlet }        from "react-router-dom"
+import Footer            from "./components/Footer"
+import Nav               from "./components/Nav"
 import "./css/App.scss"
 
-// dotenv.config()
-
-// const app       = ini"tializeApp( {
-//                                    apiKey           : process.env.TOKEN,
-//                                    appId            :
-// "1:999810587608:web:3891f2572150f6634270ad", authDomain       :
-// "waldo-693b9.firebaseapp.com", messagingSenderId: "999810587608", projectId
-//       : "waldo-693b9", storageBucket    : "waldo-693b9.appspot.com", },
-// "waldo" ) window.firebase = app
-const App = () => {
+const firebaseConfig = {
+// @ts-ignore
+  apiKey           : import.meta.env.VITE_API_KEY,
+  authDomain       : "invidious-530a1.firebaseapp.com",
+  projectId        : "invidious-530a1",
+  storageBucket    : "invidious-530a1.appspot.com",
+  messagingSenderId: "1060599399250",
+  appId            : "1:1060599399250:web:ad52deb2120f6671d5b08b",
+}
+// @ts-ignore
+window.firebaseApp   = initializeApp( firebaseConfig )
+const App            = () => {
   return ( <>
       <Nav/>
       <main>
