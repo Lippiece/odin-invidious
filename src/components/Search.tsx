cce31284cee3244fd08dd1ecee34fa0ebe268b0f
lippiece <github@lippiece.anonaddy.me>
2023-05-15 17:12:42 +0200

    feat: search UX
    Search on enter, searchbar state.

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,17 +2,23 @@ import { Classes, Icon }                               from "@blueprintjs/core"
 import { ContentTypes, fetchInstances, searchContent } from "@invidjs/invid-js"
 import { useAtom }                                     from "jotai"
 import { useState }                                    from "react"
+import { useNavigate }                                 from "react-router-dom"
 import handleError
                                                        from "../logic/handleError"
 import { errorAtom, searchAtom }                       from "../state/atoms"
 
 const Search = () => {
-  const [ , setError ] = useAtom( errorAtom )
-
-  const [ query, setQuery ]    = useState( "" )
+  const [ , setError ]         = useAtom( errorAtom )
   const [ , setSearchResults ] = useAtom( searchAtom )
+  const navigate               = useNavigate()
 
-  const search = async ( _, tryNumber = 0 ) => {
+  const [ query, setQuery ]   = useState( "" )
+  const [ status, setStatus ] = useState( "" )
+
+  const search = async ( _ = {}, tryNumber = 0 ) => {
+    if ( Boolean( status ) ) return
+
+    setStatus( "fetching" )
     const instances = await fetchInstances()
     const instance  = instances.find( ( { api_allowed } ) => api_allowed )
     try {
@@ -23,18 +29,23 @@ const Search = () => {
       )
       // @ts-ignore
       setSearchResults( response )
+      setStatus( "" )
+      navigate( "/odin-invidious/main" )
       return response
     } catch ( error ) {
       handleError( error )( setError )
+      setStatus( "" )
       return []
     }
   }
 
   return (
     <>
-      <div className={ Classes.INPUT_GROUP }>
+      <div
+        className={ Classes.INPUT_GROUP }
+      >
         <Icon
-          icon="search"
+          icon={ status === "fetching" ? "dot" : "search" }
           onClick={ search }
         />
         <input
@@ -43,6 +54,8 @@ const Search = () => {
           value={ query }
           placeholder="Search"
           onChange={ ( event ) => setQuery( event.target.value ) }
+          onKeyDown={ ( event ) => event.key === "Enter" && search() }
+          disabled={ Boolean( status ) }
         >
         </input>
       </div>
